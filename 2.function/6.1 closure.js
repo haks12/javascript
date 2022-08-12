958b1c36779ad21d0fa0628f42ee4664bde32d3a
unknown <a@a.com>
2022-08-12 18:26:46 +0900

    '0812'

--- 2.function/6.1 closure.js
+++ 2.function/6.1 closure.js
@@ -1,4 +1,8 @@
 const age = 16
+//sayAge() // TypeError: sayAge is not a function
+//tellAge() // ReferenceError: tellAge is not defined
+console.log(sayAge) //ReferenceError 아니다. undefined
+//console.log(hello) // ReferenceError: hello is not defined
 
 { //lexical environment 생성(또 다른 네모)
     let age = 10
@@ -14,5 +18,8 @@ const age = 16
     sayAge()
     tellAge()
 }
-
+sayAge() // 10
+//tellAge() // ReferenceError
 console.log(age)
+//js에서는 function도 객체이다.
+//closure 외부변수를 참조할 수 있는 function
---
+++ 2.function/6.2 closure.js
@@ -0,0 +1,22 @@
+const age = 16
+//sayAge() // ReferenceError: sayAge is not defined
+//tellAge() // ReferenceError
+
+function init() {
+    let age = 10
+
+    function sayAge() {
+        console.log(age)
+    }
+
+    let tellAge = function() {
+        console.log(age)
+    }
+
+    sayAge()
+    tellAge()
+}
+
+init()
+//sayAge() // ReferenceError
+//tellAge() // ReferenceError
